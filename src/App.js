2fb2a28d8ac2d4197d23052c356b6060a036810a
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-05-15 16:33:10 +0530

    Todocreate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Icon } from 'semantic-ui-react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(){
+      super();
+      this.state={
+        show:false,
+        userName:'',
+        storeUser:[],
+        isEdit:false,
+      }
+    }
+
+    handleChange = (e) => {
+      this.setState({
+        userName:e.target.value,
+        show:true
+      })
+    }
+
+    handlesubmit = (e) => {
+      e.preventDefault();
+      const {storeUser, userName} = this.state;
+      this.setState({
+        storeUser: [...storeUser, {id:Date.now(), myName:userName} ],
+      })
+      
+    }
+
+    handleDelete = (id) =>{
+      this.setState({
+        storeUser: this.state.storeUser.filter((data) => data.id !== id)
+      })
+    }
+
+    handleEdit = (ind) =>{
+      this.setState({
+        isEdit:{
+          [ind]:!this.state.isEdit[ind]
+        }
+      })
+    }
+
+    
+
+    HandleEditChange = (e, id) =>{
+      this.setState({
+        storeUser:this.state.storeUser.map(todo => {
+          if(todo.id === id){
+            todo.myName= e.target.value
+          }
+          return todo;
+        })
+      })
+    }
+
+
+    render(){
+      const {show, storeUser, isEdit} = this.state;
+      return(
+        <>
+          
+          <form>
+          <h2 style={{margin:"30px auto", textAlign:"center"}}>Webshine Todo's</h2>
+          <div className="myInput_boxmain">
+            <div className="myInput_box">
+              <input type="text" placeholder="Add Names" onChange={(e)=>this.handleChange(e)} />
+            </div>
+            <div className="myInput_box">
+              <span className={show ? 'active':'deactive'}><Icon name="plus" color="blue" size="large" onClick={this.handlesubmit} /></span>
+            </div>
+          </div>
+          </form>
+          
+          {storeUser.map((data, i) => {
+            return(
+              <div key={data.id}>
+                {isEdit[i] ?
+                <div className="myInput_edit">
+                  {/* <h4>{data.id}</h4> */}
+                  <div className="myInput_box_edit">
+                  <input type="text" value={data.myName} onChange={(e, i)=>this.HandleEditChange(e, data.id)} />
+                  </div>
+                  
+                  <div className="myInput_box_edit">
+                  <Icon className="myInput_icon" name="save" color="blue" size="large"  onClick={() => this.handleEdit(i)} />
+                  </div>
+                </div>
+                :
+                <div className="myInput_edit">
+                {/* <h4>{data.id}</h4> */}
+                    <div className="myInput_box">
+                      <h2>{data.myName}</h2>
+                    </div>
+                    <div className="myInput_box_edit">
+                    <Icon className="myInput_icon" name="cancel" color="blue" size="large" onClick={()=>this.handleDelete(data.id)} />
+                    </div>
+                    <div className="myInput_box_edit">
+                    <Icon className="myInput_icon" name="edit" color="blue" size="large" onClick={() => this.handleEdit(i)} />
+                    </div>
+                </div>
+                }
+              </div>
+            )
+          })}
+        </>
+      )
+    }
 }
 
 export default App;
